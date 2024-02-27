b393e9c8dd041ee81fb2056504b77ae53d954673
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 15:29:14 -0500

    Update 1_📈_Plotting_Demo.py

--- app/pages/1_📈_Plotting_Demo.py
+++ app/pages/1_📈_Plotting_Demo.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+     [0.1, 0.5, 1.0],
+     [0.1, 0.5, 0.9])
 
 st.line_chart(chart_data)
