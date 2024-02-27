f45353018cc3e9eee0ad92720210c742510a377e
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 15:03:21 -0500

    Delete pages/pages/pages directory

--- pages/pages/pages/1_📈_Plotting_Demo.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show_page1():
-    st.title("Python Code for the Project")
-
-    code = """
-    # Your Python code for the project goes here
-    """
-    st.code(code, language='python')
