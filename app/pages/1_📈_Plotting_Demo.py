ae117cb00dde6e3d13dfffd7e7cbffdc84640727
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 20:36:17 -0500

    Update 1_📈_Plotting_Demo.py

--- app/pages/1_📈_Plotting_Demo.py
+++ app/pages/1_📈_Plotting_Demo.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.express as px
 
 
-df = pd.read_csv("rmseplt")
+
+df = pd.read_csv("rmseplt.csv")
 chart_data = pd.DataFrame(df["Epochs"], df["rmse_values_train"])
 
 st.line_chart(chart_data)
