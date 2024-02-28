13733e8496abc6abb69fa497f98d012ba2e36b27
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 20:51:50 -0500

    Update 1_📈_Plotting_Demo.py

--- app/pages/1_📈_Plotting_Demo.py
+++ app/pages/1_📈_Plotting_Demo.py
@@ -3,16 +3,14 @@ import numpy as np
 import pandas as pd
 
 
+Data = "https://github.com/AliElDahhan/AliElDahhan.github.io/blob/master/app/pages/rmseplt.csv"
+data = pd.read_csv(DATA_URL)
 
-chart_data = pd.DataFrame([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10], [7.944234761966651,
- 7.344119762294723,
- 7.1997220161386215,
- 7.151222034236081,
- 7.121901498210893,
- 7.099689608510607,
- 7.0823707166259675,
- 7.06882998027756,
- 7.058211810170119,
- 7.0498692020033005])
+chart_data = pd.DataFrame(
+   {
+      data["Epochs"], data["rmse_values_train"] 
+ 
+)
 
 st.line_chart(chart_data)
+
