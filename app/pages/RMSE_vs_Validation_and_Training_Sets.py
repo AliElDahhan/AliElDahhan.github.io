7dff33c3e917a88b233d6425489ac65617516334
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:10:41 -0500

    Update RMSE_vs_Validation_and_Training_Sets.py

--- app/pages/RMSE_vs_Validation_and_Training_Sets.py
+++ app/pages/RMSE_vs_Validation_and_Training_Sets.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], columns=["[1,2]", "col2", "col3"])
+chart_data = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], columns=["Epochs", "col2", "col3"])
 
 st.line_chart(
    chart_data, x="col1", y=["col2", "col3"], color=["#FF0000", "#0000FF"]  # Optional
