1368ecfef745027eb3bb97e2e284e9e81c3edc03
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 21:02:48 -0500

    Update 1_📈_Plotting_Demo.py

--- app/pages/1_📈_Plotting_Demo.py
+++ app/pages/1_📈_Plotting_Demo.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 
-chart_data = pd.DataFrame([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10], columns=["col1", "col2", "col3"])
+chart_data = pd.DataFrame([ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10], columns=["Epochs", "rmse_values_train", "rmse_values_vaild"])
 
 st.line_chart(
-   chart_data, x="col1", y=["col2", "col3"], color=["#FF0000", "#0000FF"]  # Optional
+   chart_data, x="Epochs", y=["rmse_values_train", "rmse_values_vaild"], color=["#FF0000", "#0000FF"]  # Optional
 )
 
 
