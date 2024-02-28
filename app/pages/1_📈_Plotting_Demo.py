bfde3516a9649ef71f996100a54f1c8ff5cefbab
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 20:19:50 -0500

    Update 1_📈_Plotting_Demo.py

--- app/pages/1_📈_Plotting_Demo.py
+++ app/pages/1_📈_Plotting_Demo.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 DATE_COLUMN1 = 'Epochs'
 DATE_COLUMN2 = 'rmse_values_train	'
-DATA_URL = (https://github.com/AliElDahhan/AliElDahhan.github.io/blob/master/app/pages/rmseplt.csv)
+DATA_URL = ('https://github.com/AliElDahhan/AliElDahhan.github.io/blob/master/app/pages/rmseplt.csv')
 
 chart_data = pd.DataFrame(
      DATE_COLUMN1,DATE_COLUMN2)
