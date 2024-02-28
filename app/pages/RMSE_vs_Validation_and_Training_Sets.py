8feacf7a7546f931989565c547a3623560d50226
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:26:00 -0500

    Update RMSE_vs_Validation_and_Training_Sets.py

--- app/pages/RMSE_vs_Validation_and_Training_Sets.py
+++ app/pages/RMSE_vs_Validation_and_Training_Sets.py
@@ -5,5 +5,5 @@ import numpy as np
 chart_data = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], columns=["col1", "col2", "col3"])
 
 st.line_chart(
-   chart_data, x="col1", y=["col2", "col3"], color=["#FF0000", "#0000FF"]  # Optional
+   chart_data, x=df["Epochs"], y=df["Epochs"], color=["#FF0000", "#0000FF"]  # Optional
 )
