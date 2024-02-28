da22165b7c10fb052bc939219b2d51694c9d1124
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 20:24:22 -0500

    Update 1_📈_Plotting_Demo.py

--- app/pages/1_📈_Plotting_Demo.py
+++ app/pages/1_📈_Plotting_Demo.py
@@ -2,11 +2,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv("./app/pages/rmseplt.csv")
 DATE_COLUMN1 = 'Epochs'
 DATE_COLUMN2 = 'rmse_values_train	'
-DATA_URL = ('https://github.com/AliElDahhan/AliElDahhan.github.io/blob/master/app/pages/rmseplt.csv')
+
 
 chart_data = pd.DataFrame(
-     DATE_COLUMN1,DATE_COLUMN2)
+     df.iloc[:, 0],df.iloc[:, 1])
 
 st.line_chart(chart_data)
