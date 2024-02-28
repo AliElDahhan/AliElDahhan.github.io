eb4a829641fe4ab5e8cb9a03cb9b521a623b1aba
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:28:39 -0500

    Update RMSE_vs_Validation_and_Training_Sets.py

--- app/pages/RMSE_vs_Validation_and_Training_Sets.py
+++ app/pages/RMSE_vs_Validation_and_Training_Sets.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.line_chart(
-   chart_data, x=df["Epochs"], y=df["Epochs"], color=["#FF0000", "#0000FF"]  # Optional
-)
+chart_data = pd.DataFrame("rmseplt.csv", columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
