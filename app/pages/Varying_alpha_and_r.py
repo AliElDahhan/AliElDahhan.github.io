08c4936eb94f4198ca657e3dccb5124fa9d84c09
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:51:52 -0500

    Delete app/pages/Varying_alpha_and_r.py

--- app/pages/Varying_alpha_and_r.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('rmseplt.csv')
-
-st.line_chart(df.set_index('Epochs')[['rmse_values_train', 'rmse_values_valid']])
