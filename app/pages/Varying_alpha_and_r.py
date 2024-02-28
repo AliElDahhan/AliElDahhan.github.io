379dc327e4e1f9e1e9ee36538e5f289bdb25cf5c
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:03:13 -0500

    Update Varying_alpha_and_r.py

--- app/pages/Varying_alpha_and_r.py
+++ app/pages/Varying_alpha_and_r.py
@@ -3,7 +3,7 @@ import streamlit as st
 def show_page1():
     st.title("Python Code for the Project")
 
-    code = """
+    code = "sakjdksjaljdlsajd""
     # Your Python code for the project goes here
     """
     st.code(code, language='python')
