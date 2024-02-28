a91ef938ce602a6890be9de2aca6ee4312835b05
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:36:04 -0500

    Update RMSE_vs_Validation_and_Training_Sets.py

--- app/pages/RMSE_vs_Validation_and_Training_Sets.py
+++ app/pages/RMSE_vs_Validation_and_Training_Sets.py
@@ -1,7 +1,9 @@
+# page2.py
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-chart_data = pd.DataFrame("rmseplt.csv", columns=["a", "b", "c"])
-
-st.line_chart(chart_data)
+def app():
+    st.title("Page 2")
+    # Your code for Page 2 (e.g., plotting RMSE values)
+    df = pd.read_csv('rmseplt.csv')
+    st.line_chart(df.set_index('Epochs')[['rmse_values_train', 'rmse_values_valid']])
