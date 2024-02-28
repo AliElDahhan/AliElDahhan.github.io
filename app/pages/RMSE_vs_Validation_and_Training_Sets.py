4478f2bbed66fab54112bb7f9296eedc450cabd3
Ali Eldahhan <83871405+AliElDahhan@users.noreply.github.com>
2024-02-27 23:38:38 -0500

    Update RMSE_vs_Validation_and_Training_Sets.py

--- app/pages/RMSE_vs_Validation_and_Training_Sets.py
+++ app/pages/RMSE_vs_Validation_and_Training_Sets.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 def app():
     st.title("Page 2")
-    # Your code for Page 2 (e.g., plotting RMSE values)
-    df = pd.read_csv('rmseplt.csv')
+    # Load and display the data
+    df = pd.read_csv('rmseplt (1).csv')
+    st.write(df)  # Check if the data is displayed correctly
+
+    # Plot the RMSE values
     st.line_chart(df.set_index('Epochs')[['rmse_values_train', 'rmse_values_valid']])
